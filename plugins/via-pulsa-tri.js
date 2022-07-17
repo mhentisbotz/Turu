93100ab2fb77c5743be5317e868a291b1228bd37
mhentisbotz <109332967+mhentisbotz@users.noreply.github.com>
2022-07-17 11:21:32 +0700

    Update via-pulsa-tri.js

--- plugins/via-pulsa-tri.js
+++ plugins/via-pulsa-tri.js
@@ -4,19 +4,11 @@ let text = `${htki} TRI3 ${htka}
 Hay👋, ingin melanjutkan pembayaran?
 
 💰 *Pembayaran*
-• Via: Pulsa Tri3
-• Nomor: 088215689772
-• A/n: Fangz
-• Mitra: Fangz BOT
+• Via: Pulsa Indosat
+• Nomor: 085646606905
+• A/n: Asyraf
+• Mitra: MikAzu
 • Metode pembayaran: Online ( ~Cod~ )
-
-💰 *Bayar melalui aplikasi TokoBot*
-
-TokoBot: https://app.bukaolshop.com/toko/tokobot
-
-FangzApp:https://web.jagel.id/store/fangzapp
-
-Website:https://toko.ly/TokoBot   (Non apk)
 
 📦 _Informasi Pembayaran_
 
@@ -27,8 +19,8 @@ Pastikan pulsa kamu mencukupi untuk bertransaksi!
 📮KLIK *SUDAH BAYAR* JIKA SUDAH MEMBAYAR!
 `
 const templateButtons = [
-    {index: 1, urlButton: {displayText: 'Isi Pulsa', url: 'KONTER LAH ANJ'}},
-    {index: 4, quickReplyButton: {displayText: 'Sudah membayar', id: '.sudahbayar'}},
+    {index: 1, urlButton: {displayText: 'Isi Pulsa', url: 'KE KONTER LAH ANJ'}},
+    {index: 4, quickReplyButton: {displayText: 'SUDAH BAYAR', id: '.sudahbayar'}},
 ]
 let tm = {
 text: text,
